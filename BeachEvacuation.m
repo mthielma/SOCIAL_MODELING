93d0b202a6992ffbce43858ca53d53cf7795a68c
Marcel Thielmann <marcel.thielmann@erdw.ethz.ch>
2011-10-12 21:55:46 +0200

    started tracking

---
+++ BeachEvacuation.m
@@ -0,0 +1,68 @@
+function [STATISTICS, AGENTS] = BeachEvacuation(INPUT,AGENTS,BUILDINGS,FLOOD,TOPOGRAPHY)
+
+
+
+% initialize buildings
+% - location
+% - number of people inside
+% - number of families inside
+% - location of the exit
+
+
+
+% initialize agents in buildings and on streets
+% - location
+% - mass
+% - age
+% - group
+% - helping factor
+% - panic factor ()
+% - reaction factor
+% - maximum velocity
+
+
+
+
+
+% plot everything
+
+
+
+%==========================================================================
+% time loop
+
+
+%----------------------------------------------------
+% compute forces from buildings
+%----------------------------------------------------
+
+%----------------------------------------------------
+% compute forces from exits
+%----------------------------------------------------
+
+%----------------------------------------------------
+% compute forces from flood
+%----------------------------------------------------
+
+%----------------------------------------------------
+% compute forces from other agents
+%----------------------------------------------------
+
+
+%----------------------------------------------------
+% compute decisions of agents
+% - direction
+% - help others
+%----------------------------------------------------
+
+
+%----------------------------------------------------
+% compute velocity of agents
+%----------------------------------------------------
+
+
+%----------------------------------------------------
+% move agents
+%----------------------------------------------------
+
+%==========================================================================
