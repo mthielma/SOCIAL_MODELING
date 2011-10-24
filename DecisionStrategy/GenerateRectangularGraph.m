92a2d46b1e1a3eb191ab6ba68d802a51412db5fe
Marcel Thielmann <marcel.thielmann@erdw.ethz.ch>
2011-10-24 18:49:49 +0200

    cleaned up and added gradient and distance computation

--- DecisionStrategy/GenerateRectangularGraph.m
+++ DecisionStrategy/GenerateRectangularGraph.m
@@ -1,50 +1,26 @@
-function [X,Y,Number,GCOORD,RegularElementNumber,ELEM2NODE,ElemArea] = GenerateRectangularGraph(xvec,yvec)
-% generate graph for a rectangular net of streets. Paths can be added later
+function [X,Y,PathVec,Distance,Gradient] = GenerateRectangularGraph(XGrid,YGrid,ZGrid)
+% generate graph for a rectangular net of streets.
+% additionally compute topography differences
 
+% INPUT: rectangular grids for x,y,z
 if nargin == 0
    xvec = 0:10;
    yvec = 0:10;
+   [XGrid,YGrid] = meshgrid(xvec,yvec);
+   ZGrid         = XGrid*0;
 end
-% INPUT:
-% xvec, yvec: vectors that contain the coordinates of the physical grid
 
-% OUTPUT:
-% X,Y : structure that contains the x- or z- coordinates for the staggered
-% grid, fields are: Qx, Qy, H_center, H_interp 
-% 
-% Number
+[Ny,Nx] = size(XGrid);
 
-
-Nx = length(xvec);
-Ny = length(yvec);
 
 %------------------------------
-% create coordinate matrices
+% create coordinate vectors
+% use matlabs linear indexing for that
 %------------------------------
-yvec = yvec';
+X = XGrid(:);
+Y = YGrid(:);
+Z = ZGrid(:);
 
-% Corner point grid
-X.grid = repmat(xvec,Ny,1);
-Y.grid = repmat(yvec,1,Nx);
-
-%Qx-grid 
-X.Qx	  = [X.grid(2:Ny,:)+X.grid(1:Ny-1,:)]/2;
-Y.Qx	  = [Y.grid(2:Ny,:)+Y.grid(1:Ny-1,:)]/2;
-
-%Vz-Grid
-X.Qy	  = [X.grid(:,2:Nx)+X.grid(:,1:Nx-1)]/2;
-Y.Qy	  = [Y.grid(:,2:Nx)+Y.grid(:,1:Nx-1)]/2;
-
-%P-grid
-X.Center	  = [X.grid(2:Ny,2:Nx)+X.grid(1:Ny-1,1:Nx-1)]/2;
-Y.Center	  = [Y.grid(2:Ny,2:Nx)+Y.grid(1:Ny-1,1:Nx-1)]/2;
-
-
-% area of elements
-dx_elem = X.Qx(:,2:end)-X.Qx(:,1:end-1);
-dz_elem = Y.Qy(2:end,:)-Y.Qy(1:end-1,:);
-
-ElemArea = dx_elem.*dz_elem;
 %------------------------------
 % create numbering scheme
 %------------------------------
@@ -53,7 +29,7 @@ Number_ind        =   zeros(Ny + Ny-1, Nx + Nx-1);  % Create the general numberi
 Number.PathY      =   zeros(Ny-1,Nx  );
 Number.PathX      =   zeros(Ny  ,Nx-1);
 Number.Center     =   zeros(Ny-1,Nx-1);
-Number.Corner     =   zeros(Ny,Nx);
+Number.Nodes     =   zeros(Ny,Nx);
 
 for ix=1:2:Nx+Nx-1, for iz=2:2:Ny+Ny-1, Number.Phase(iz,ix) = 2; end; end % Qx equations
 for ix=2:2:Nx+Nx-1, for iz=1:2:Ny+Ny-1, Number.Phase(iz,ix) = 3; end; end % Qz equations
@@ -82,7 +58,18 @@ for ix=1:size(Number.Phase,2)         % x horizontal direction
     end
 end
 
+% %Center-numbering
+% for ix=1:size(Number.Phase,2)         % x horizontal direction
+%     for iz=1:size(Number.Phase,1)     % z vertical direction
+%         if Number.Phase(iz,ix)==4
+%             Number_ind(iz,ix)   =   num;    % Number_ind numbers all the variables (Vx,Vz,P) in the grid
+%             num                 =   num+1;
+%         end
+%     end
+% end
+
 num = 1;
+
 %corner point-numbering
 for ix=1:size(Number.Phase,2)         % x horizontal direction
     for iz=1:size(Number.Phase,1)     % z vertical direction
@@ -92,18 +79,6 @@ for ix=1:size(Number.Phase,2)         % x horizontal direction
         end
     end
 end
-
-% %Center-numbering
-% for ix=1:size(Number.Phase,2)         % x horizontal direction
-%     for iz=1:size(Number.Phase,1)     % z vertical direction
-%         if Number.Phase(iz,ix)==3
-%             Number_ind(iz,ix)   =   num;    % Number_ind numbers all the variables (Vx,Vz,P) in the grid
-%             num                 =   num+1;
-%         end
-%     end
-% end
-
-
 
 num_eqns         =   num-1;
 % Now num_eqns is the total number of points of Vx, Vz and P, and thus number of equations
@@ -117,43 +92,17 @@ num_eqns         =   num-1;
 ind_PathY    =   find(Number.Phase==2);  Number.PathY(find(Number.PathY==0)) = Number_ind(ind_PathY);
 ind_PathX    =   find(Number.Phase==3);  Number.PathX(find(Number.PathX==0)) = Number_ind(ind_PathX);
 ind_Center =   find(Number.Phase==4);  Number.Center(find(Number.Center ==0)) = Number_ind(ind_Center );
-ind_Corner =   find(Number.Phase==1);  Number.Corner(find(Number.Corner ==0)) = Number_ind(ind_Corner );
-
-% create a complete grid with all coordinates
-X.complete = zeros(size(Number.Phase));
-Y.complete = zeros(size(Number.Phase));
-
-X.complete(ind_PathY) = X.Qx;
-Y.complete(ind_PathY) = Y.Qx;
-
-X.complete(ind_PathX) = X.Qy;
-Y.complete(ind_PathX) = Y.Qy;
-
-X.complete(ind_Center)  = X.Center;
-Y.complete(ind_Center)  = Y.Center;
-
-X.complete(ind_Corner) = X.grid;
-Y.complete(ind_Corner) = Y.grid;
+ind_Corner =   find(Number.Phase==1);  Number.Nodes(find(Number.Nodes ==0)) = Number_ind(ind_Corner );
 
 % create a complete number grid with all numbers
 Number.complete = zeros(size(Number.Phase));
-Number.complete = zeros(size(Number.Phase));
 
 Number.complete(ind_PathY) = Number.PathY;
-Number.complete(ind_PathY) = Number.PathY;
-
 Number.complete(ind_PathX) = Number.PathX;
-Number.complete(ind_PathX) = Number.PathX;
-
 Number.complete(ind_Center)  = Number.Center;
-Number.complete(ind_Center)  = Number.Center;
-
-%Number.complete(ind_Corner) = Number.Corner;
-%Number.complete(ind_Corner) = Number.Corner;
-
 
 NumberNodes = zeros(size(Number.Phase));
-NumberNodes(ind_Corner) = Number.Corner;
+NumberNodes(ind_Corner) = Number.Nodes;
 % create vector that contains the node numbers per path (the row index represents the path)
 PathVec = zeros(max(Number.complete(:)),2);
 
@@ -173,12 +122,30 @@ for iPath = 1:length(Number.PathY(:));
     PathVec(Number.PathY(iPath),2) = NumberNodes(iz+1,ix); % right   
 end
 
-
-Number.complete(ind_Corner) = Number.Corner;
-Number.complete(ind_Corner) = Number.Corner;
+% diagonal paths (not used here, therefore we delete them)
 
 
-bla;
+
+
+%==========================================================================
+% for each path, compute the topography gradient
+
+% to do this, look at each path and compute the height difference between
+% the nodes
+% the Number.Nodes structure contains the numbers that can then be related
+% to the coordinates. Since the 
+% e.g.
+% X(PathVec,1) gives the x-coordinates for the starting nodes
+% X(PathVec,2) gives the x-coordinates for the ending nodes
+
+
+
+Distance   =  sqrt((X(PathVec(:,2))-X(PathVec(:,1))).^2+(Y(PathVec(:,2))-Y(PathVec(:,1))).^2); % compute distance between the nodes
+DiffHeight =  Z(PathVec(:,2))-Z(PathVec(:,1)); % compute height difference between nodes, positive:upwards, negative: downwards 
+Gradient   =  DiffHeight./Distance;
+
+%==========================================================================
+
 
 
 
