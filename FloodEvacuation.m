bcdac16e3299300310220a9e6d74ccac30a81144
Marcel Thielmann <marcel.thielmann@erdw.ethz.ch>
2011-10-27 10:17:35 +0200

    added removal of paths due to buildings added some test case information (grid and buildings)

--- FloodEvacuation.m
+++ FloodEvacuation.m
@@ -1,7 +1,26 @@
-function [STATISTICS, AGENTS] = FloodEvacuation(INPUT,AGENTS,BUILDINGS,STREETS,FLOOD,TOPOGRAPHY)
+%function [STATISTICS, AGENTS] = FloodEvacuation(INPUT,AGENTS,BUILDINGS,STREETS,FLOOD,GRID,TOPOGRAPHY)
 
 
 %%%% FABIO SUPERSTAR !!!!!!!!!!!!!!
+Debug = 0;
+
+
+addpath ./DecisionStrategy/
+addpath ./WallForces/
+%==========================================================================
+% initialize fine grid (if not given as argument)
+
+xvec = 0:100;
+yvec = 0:100;
+[X_Grid,Y_Grid] = meshgrid(xvec,yvec);
+Z_Grid = 0*X_Grid; % no topography
+
+% initialize coarse grid for road network
+xRoad = 10:10:90;
+yRoad = 10:4:90;
+[XRoad,YRoad] = meshgrid(xRoad,yRoad);
+ZRoad = interp2(X_Grid,Y_Grid,Z_Grid,XRoad,YRoad,'linear');
+
 
 
 %==========================================================================
@@ -11,6 +30,15 @@ function [STATISTICS, AGENTS] = FloodEvacuation(INPUT,AGENTS,BUILDINGS,STREETS,F
 % - number of families inside
 % - location of the exit
 
+%---------------------------------------
+% test: create one building in the middle
+%---------------------------------------
+BuildingList = [42 49 42 49;...
+                20 40 12 30]; % coordinates of building xmin xmax ymin ymax
+BuildingMap = X_Grid*0;
+for i = 1:size(BuildingList,1)
+    BuildingMap(X_Grid>=BuildingList(i,1) & X_Grid<=BuildingList(i,2) & Y_Grid>=BuildingList(i,3) & Y_Grid<=BuildingList(i,4)) = 1;
+end
 
 %----------------------------------------------------
 % compute forces from buildings (static)
@@ -23,9 +51,11 @@ function [STATISTICS, AGENTS] = FloodEvacuation(INPUT,AGENTS,BUILDINGS,STREETS,F
 % - cost due to street width
 % - cost due to street type (paved, unpaved)
 % - add up all costs
-[X,Y,PathVec,Distance,Gradient] = GenerateRectangularGraph(XGrid,YGrid,ZGrid);
-% delete paths at locations where there are buildings
 
+[X,Y,PathVec,Distance,Gradient] = GenerateRectangularGraph(XRoad,YRoad,ZRoad);
+
+% delete paths at locations where there are buildings covering the nodes
+[PathVec,Distance,Gradient] = RemovePaths(X_Grid,Y_Grid,BuildingMap,X,Y,PathVec,Distance,Gradient);
 
 %==========================================================================
 % initialize agents in buildings and on streets
@@ -36,10 +66,6 @@ function [STATISTICS, AGENTS] = FloodEvacuation(INPUT,AGENTS,BUILDINGS,STREETS,F
 % - horizon distance (how far can the agent look?)
 % - group (e.g. family) --> later
 % - helping factor --> later
-
-
-% plot everything
-
 
 
 %==========================================================================
@@ -76,7 +102,12 @@ for itime = 1:nt
         
         %----------------------------------------------------
         % get the agents in a certain range using the kdtree
+        %
+        % Alternative: use agents that are on the same road
+        % since we track this, we do not need kdtree
         %----------------------------------------------------
+        
+        
         
         %----------------------------------------------------
         % compute social forces from other agents and apply a weighting
@@ -99,6 +130,26 @@ for itime = 1:nt
         % - the agent has reached the destination point
         % - the destination point or a part of the path are flooded
         %----------------------------------------------------
+        
+        if Decision
+           % add the paths between the agent and the next crossings (nodes)
+           % to the path vector
+           maxNode   = max(PathVec(:));
+           NodeAgent = maxNode+1;
+           
+           [NodeAgent NodeEnd]
+           [NodeAgent NodeStart]
+           
+           % compute the additional cost due to people in the way
+           
+           
+           
+           % Find the shortest path via a MATLAB function
+           % Using the Bioinformatics Toolbox? function graphshortestpath the shortest
+           % path between [S = Starting Node] and [T = Final Node] can be found using
+           %[dist, path, pred] = graphshortestpath(G, S, T)
+           [dist, path, pred] = graphshortestpath(nNet,1,20,'directed',true);
+        end
         
         
         %----------------------------------------------------
