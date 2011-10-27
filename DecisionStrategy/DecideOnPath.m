f5ebe623c91c0f2874b4f1a29331caec064e4bad
Marcel Thielmann <marcel.thielmann@erdw.ethz.ch>
2011-10-27 18:30:42 +0200

    fixed a bug

--- DecisionStrategy/DecideOnPath.m
+++ DecisionStrategy/DecideOnPath.m
@@ -1,24 +1,24 @@
-function Agent = DecideOnPath(iagent,Agent,PathVec,GradientFactor,Distance,X,Y,Z)
+function Agent = DecideOnPath(Agent,PathVec,GradientFactor,Distance,X,Y,Z)
 
-PathWeights = Distance./(AGENT(iagent).VelMax*GradientFactor);
+PathWeights = Distance./(Agent.VelMax*GradientFactor);
 
 % add the paths between the agent and the next crossings (nodes)
 % to the path vector
 maxNode   = max(PathVec(:));
 NodeAgent = maxNode+1;
-NodeEnd   = PathVec(AGENT(iagent).PresentPath,2);
-NodeStart = PathVec(AGENT(iagent).PresentPath,1);
+NodeEnd   = PathVec(Agent.PresentPath,2);
+NodeStart = PathVec(Agent.PresentPath,1);
 
-DistanceEnd      = sqrt((AGENT(iagent).LocX-X(NodeEnd)).^2+(AGENT(iagent).LocY-Y(NodeEnd)).^2);
-DistanceStart    = sqrt((AGENT(iagent).LocX-X(NodeStart)).^2+(AGENT(iagent).LocY-Y(NodeStart)).^2);
-GradientEnd      = AGENT(iagent).LocZ-Z(NodeEnd);
-GradientStart    = AGENT(iagent).LocZ-Z(NodeStart);
+DistanceEnd      = sqrt((Agent.LocX-X(NodeEnd)).^2+(Agent.LocY-Y(NodeEnd)).^2);
+DistanceStart    = sqrt((Agent.LocX-X(NodeStart)).^2+(Agent.LocY-Y(NodeStart)).^2);
+GradientEnd      = Agent.LocZ-Z(NodeEnd);
+GradientStart    = Agent.LocZ-Z(NodeStart);
 
 GradientFactorEnd   = exp(-3.5*abs(GradientEnd+0.05));
 GradientFactorStart = exp(-3.5*abs(GradientStart+0.05));
 
-WeightEnd        = DistanceEnd./(AGENT(iagent).VelMax*GradientFactorEnd);
-WeightStart      = DistanceStart./(AGENT(iagent).VelMax*GradientFactorStart);
+WeightEnd        = DistanceEnd./(Agent.VelMax*GradientFactorEnd);
+WeightStart      = DistanceStart./(Agent.VelMax*GradientFactorStart);
 
 % compute the additional cost due to people in the way
 
@@ -37,12 +37,12 @@ PathMat            = sparse(NewPathVec(:,1),NewPathVec(:,2),NewWeightVec(:));
 % path between [S = Starting Node] and [T = Final Node] can be found using
 %[dist, path, pred] = graphshortestpath(G, S, T)
 time_est = realmax;
-for i = 1:length(AGENT(iagent).ExitNode)
-    EndNode   = AGENT(iagent).ExitNode;
+for i = 1:length(Agent.ExitNode)
+    EndNode   = Agent.ExitNode;
     [time_est2, path, pred] = graphshortestpath(PathMat,StartNode,EndNode,'directed',true);
     if time_est2<time_est
         time_est = time_est2;
         % set the next destination node for the agent
-        AGENT(iagent).DestinationNode = path(2);
+        Agent.DestinationNode = path(2);
     end
 end
