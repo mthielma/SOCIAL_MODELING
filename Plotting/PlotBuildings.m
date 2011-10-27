aaf6dd5b7bf090a65070f23f98754d65de426701
Marcel Thielmann <marcel.thielmann@erdw.ethz.ch>
2011-10-27 16:18:25 +0200

    added plotting routines in a separate folder and created a gitignore file for matlab autosave files

---
+++ Plotting/PlotBuildings.m
@@ -0,0 +1,15 @@
+function PlotBuildings(BuildingList,Color)
+
+% plot buildings
+% BuildingList is a matrix with  [xmin xmax ymin ymax] per row
+% Color: FaceColor of the building rectangle
+%
+% Marcel Thielmann Oct 2011
+
+for i = 1:size(BuildingList,1)
+    x = BuildingList(i,1);
+    y = BuildingList(i,3);
+    w = BuildingList(i,2)-BuildingList(i,1);
+    h = BuildingList(i,4)-BuildingList(i,3);
+    rectangle('Position',[x y w h],'FaceColor',Color);
+end
