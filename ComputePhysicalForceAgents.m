f696ee4c91a7643a35ab28db973f03f57657689f
Marcel Thielmann <marcel.thielmann@erdw.ethz.ch>
2011-11-24 16:14:50 +0100

    removed (commented) tangential force computation

--- ComputePhysicalForceAgents.m
+++ ComputePhysicalForceAgents.m
@@ -5,8 +5,11 @@ F_physAgents_normalX = Parameter.k.*DistanceToAgents.*Normal(:,1);
 F_physAgents_normalY = Parameter.k.*DistanceToAgents.*Normal(:,2);
 % tangential force
 DeltaV      = (velx_others-velx_agent).*Tangent(:,1)+(vely_others-vely_agent).*Tangent(:,2);
-F_physAgents_tangentX = Parameter.kappa.*DistanceToAgents.*DeltaV.*Tangent(:,1);
-F_physAgents_tangentY = Parameter.kappa.*DistanceToAgents.*DeltaV.*Tangent(:,2);
+% F_physAgents_tangentX = Parameter.kappa.*DistanceToAgents.*DeltaV.*Tangent(:,1);
+% F_physAgents_tangentY = Parameter.kappa.*DistanceToAgents.*DeltaV.*Tangent(:,2);
+
+F_physAgents_tangentX = 0;
+F_physAgents_tangentY = 0;
 % add physical forces
 FxPhysAgents = F_physAgents_normalX + F_physAgents_tangentX;
 FyPhysAgents = F_physAgents_normalY + F_physAgents_tangentY; 
