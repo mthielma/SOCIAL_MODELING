5adb88b363ab35dea060f6b86725b3890530c00b
Marcel Thielmann <marcel.thielmann@erdw.ethz.ch>
2011-11-24 16:11:11 +0100

    changed the plotting radius of agents

--- Plotting/PlotAgents3D.m
+++ Plotting/PlotAgents3D.m
@@ -8,21 +8,25 @@ agent_y = [AGENT.LocY];
 agent_z = interp2(XGrid,YGrid,ZGrid,agent_x,agent_y,'*linear')+radius; % shift z up by radius
 
 
-figure(1),clf
+figure(99),clf
 hold on
 
 % plot surface
-meshz(XGrid(1:10:end,1:10:end),YGrid(1:10:end,1:10:end),ZGrid(1:10:end,1:10:end),zeros(size(XGrid(1:10:end,1:10:end)))), shading interp
+%meshz(XGrid(1:10:end,1:10:end),YGrid(1:10:end,1:10:end),ZGrid(1:10:end,1:10:end),zeros(size(XGrid(1:10:end,1:10:end))))
+surf(XGrid,YGrid,ZGrid),shading interp
+colormap('jet')
+freezeColors;
 
 for i = 1:nagents
     [xs,ys,zs] = CreateSphere(radius(i),agent_x(i),agent_y(i),agent_z(i));
     surfl(xs,ys,zs)
     shading interp
 end
+camlight left; lighting phong
 axis equal
 axis tight
-colorbar
-az = 49;
-el = 16;
+colormap('gray')
+az = 88;
+el = 15;
 view(az, el);
-zoom(2);
+zoom(3);
