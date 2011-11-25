5a84ec9178635814850d353a7332c32ed26e0b42
Marcel Thielmann <marcel.thielmann@erdw.ethz.ch>
2011-11-25 11:42:46 +0100

    - changed the function to work with the output from the new architecture force function

--- ComputeSocialForcesStatic.m
+++ ComputeSocialForcesStatic.m
@@ -1,4 +1,4 @@
 function [FxSocialStatic,FySocialStatic] = ComputeSocialForcesStatic(AGENT,X_Grid,Y_Grid,xArchForces,yArchForces,Parameter)
 
-FxSocialStatic = interp2(X_Grid,Y_Grid,xArchForces',[AGENT.LocX],[AGENT.LocY],'*linear').*exp([AGENT.Size]./Parameter.B);
-FySocialStatic = interp2(X_Grid,Y_Grid,yArchForces',[AGENT.LocX],[AGENT.LocY],'*linear').*exp([AGENT.Size]./Parameter.B);
+FxSocialStatic = interp2(X_Grid,Y_Grid,xArchForces,[AGENT.LocX],[AGENT.LocY],'*linear').*exp([AGENT.Size]./Parameter.B);
+FySocialStatic = interp2(X_Grid,Y_Grid,yArchForces,[AGENT.LocX],[AGENT.LocY],'*linear').*exp([AGENT.Size]./Parameter.B);
