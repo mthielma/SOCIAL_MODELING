281075c5c6dd8b611cf513c3f47e9b96c72caee2
Marcel Thielmann <marcel.thielmann@erdw.ethz.ch>
2011-11-29 09:33:46 +0100

    - fixed a bug that resulted in agent not going to the exits - multiple exits can now be used

--- DecisionStrategy/ComputeShortestPathGlobalWithAgents.m
+++ DecisionStrategy/ComputeShortestPathGlobalWithAgents.m
@@ -1,9 +1,9 @@
-function [Dgradx,Dgrady] = ComputeShortestPathGlobalWithAgents(BuildingList,Xexit,Yexit,X_Grid,Y_Grid,TopoGradientX,TopoGradientY,v0,resolution,AGENT,nagent,agent_sensitivity)
+function [Dgradx,Dgrady] = ComputeShortestPathGlobalWithAgents(BuildingList,Xexit,Yexit,X_Grid,Y_Grid,TopoGradientX,TopoGradientY,AGENT,nagent,Parameter)
 
 Debug = false;
 
 % set initial speed map
-F = ones(size(X_Grid))*v0;
+F = ones(size(X_Grid))*Parameter.v0;
 % add buildings to map
 for i=1:size(BuildingList,1)
     F(X_Grid>=BuildingList(i,1) & X_Grid<=BuildingList(i,2) & Y_Grid>=BuildingList(i,3) & Y_Grid<=BuildingList(i,4)) = 1e-8;
@@ -13,28 +13,32 @@ end
 % FIRST GUESS
 %==============================================================
 
-% find indices of exit
-[indx,indy] = find(X_Grid==Xexit & Y_Grid==Yexit);
-
+% find indices of exits
+ExitPoints = zeros(2,size(Xexit,1));
+for i = 1:size(Xexit,1)
+    [indx,indy] = find(X_Grid==Xexit(i) & Y_Grid==Yexit(i));
+    ExitPoints(1,i) = indx;
+    ExitPoints(2,i) = indy;
+end
 % use fast marching algorithm to compute distance to exit
-[D]=msfm(F, [indx indy]');
+[D]=msfm(F, ExitPoints);
 
 % compute gradient in time -> this gives us the direction
-[Dgradx,Dgrady] = gradient(D,resolution,resolution);
+[Dgradx_orig,Dgrady_orig] = gradient(D,Parameter.resolution,Parameter.resolution);
 
 % scale direction vectors to normal
-Dgradtot = sqrt(Dgradx.^2+Dgrady.^2);
-Dgradx   = -Dgradx./Dgradtot;
-Dgrady   = -Dgrady./Dgradtot;
+Dgradtot = sqrt(Dgradx_orig.^2+Dgrady_orig.^2);
+Dgradx_orig   = -Dgradx_orig./Dgradtot;
+Dgrady_orig   = -Dgrady_orig./Dgradtot;
 
 %==============================================================
 % RECOMPUTE WITH TOPOGRAPHY AND AGENTS
 %==============================================================
 % recompute the velocity field based on the slope
-slope = TopoGradientX.*Dgradx+TopoGradientY.*Dgrady;
+slope = TopoGradientX.*Dgradx_orig+TopoGradientY.*Dgrady_orig;
 
 % compute maximum velocity due to slope
-PreFac = (v0./exp(-3.5*0.05));
+PreFac = (Parameter.v0./exp(-3.5*0.05));
 F = PreFac.*exp(-3.5.*abs(slope+0.05));
 % add buildings to map
 for i=1:size(BuildingList,1)
@@ -47,38 +51,44 @@ F0 = F;
 % unfortunately, we have to loop over the agents so far...
 for iagent = 1:nagent
    Inside = (X_Grid-AGENT(iagent).LocX).*(X_Grid-AGENT(iagent).LocX)+(Y_Grid-AGENT(iagent).LocY).*(Y_Grid-AGENT(iagent).LocY);
-   F(Inside<AGENT(iagent).Size) = F0(Inside<AGENT(iagent).Size)*agent_sensitivity;
+   F(Inside<AGENT(iagent).Size) = F0(Inside<AGENT(iagent).Size)*0.1;
 end
 
 
 % use fast marching algorithm to compute distance to exit
-[D]=msfm(F, [indx indy]');
+[D]=msfm(F, ExitPoints);
 
 % compute gradient in time -> this gives us the direction
-[Dgradx,Dgrady] = gradient(D,resolution,resolution);
+[Dgradx_agent,Dgrady_agent] = gradient(D,Parameter.resolution,Parameter.resolution);
 
 % scale direction vectors to normal
-Dgradtot = sqrt(Dgradx.^2+Dgrady.^2);
-Dgradx   = -Dgradx./Dgradtot;
-Dgrady   = -Dgrady./Dgradtot;
+Dgradtot = sqrt(Dgradx_agent.^2+Dgrady_agent.^2);
+Dgradx_agent   = -Dgradx_agent./Dgradtot;
+Dgrady_agent   = -Dgrady_agent./Dgradtot;
 
-
-
-
-
-
+% compute mixture of both fields taking into account sensitivity to agents
+% in the way
+Dgradx = (Parameter.agent_sensitivity.*Dgradx_agent + Dgradx_orig)./(Parameter.agent_sensitivity+1);
+Dgrady = (Parameter.agent_sensitivity.*Dgrady_agent + Dgrady_orig)./(Parameter.agent_sensitivity+1);
 
 
 if Debug
-    
+
     figure(99),clf
-    pcolor(X_Grid,Y_Grid,Dgradx),shading interp
+    pcolor(X_Grid,Y_Grid,double(D)),shading interp, colorbar
+    hold on
+    quiver(X_Grid,Y_Grid,Dgradx,Dgrady,'w')
+    
     figure(98),clf
-    pcolor(X_Grid,Y_Grid,Dgrady),shading interp
+    pcolor(X_Grid,Y_Grid,double(D)),shading interp, colorbar
+    hold on
+    quiver(X_Grid,Y_Grid,Dgradx_agent,Dgrady_agent,'w')
     
     figure(97),clf
     pcolor(X_Grid,Y_Grid,double(D)),shading interp, colorbar
     hold on
-    quiver(X_Grid,Y_Grid,Dgradx,Dgrady,'w')
-    pause
+    quiver(X_Grid,Y_Grid,Dgradx_orig,Dgrady_orig,'w')
+    
+    
+    
 end
