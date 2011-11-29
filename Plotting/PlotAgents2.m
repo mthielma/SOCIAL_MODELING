539098207d7a2226acd9a81cae13ce5406ed13ec
Fabio <fabio.crameri@erdw.ethz.ch>
2011-11-29 16:30:29 +0100

    - adding numbers to agents

---
+++ Plotting/PlotAgents2.m
@@ -0,0 +1,23 @@
+function PlotAgents(nagent,AGENT,Color)
+% plot agents as circles with the size of the circle being their radius
+% AGENT is a structure that needs to have the following fields:
+% .Size: radius of the agent
+% .LocX: x-location
+% .LocY: y-location
+%
+% nagent: number of agents
+% Color: face color of the marker
+%
+% Marcel Thielmann Oct 2011
+
+for i = 1:nagent
+    radius = AGENT(i).Size;
+    x      = AGENT(i).LocX;
+    y      = AGENT(i).LocY;
+    try
+    rectangle('position',[x-radius, y-radius, 2*radius, 2*radius],'curvature',[1 1],'FaceColor',Color);
+    catch
+        bla =1;
+    end
+    text(x,y,[num2str(AGENT(i).name)],'HorizontalAlignment','center')
+end
