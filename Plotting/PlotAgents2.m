6d43a430096fe941c727187507a4a2c3a0c5fdb4
Fabio <fabio.crameri@erdw.ethz.ch>
2011-11-29 16:49:15 +0100

    Fun added

--- Plotting/PlotAgents2.m
+++ Plotting/PlotAgents2.m
@@ -1,4 +1,4 @@
-function PlotAgents(nagent,AGENT,Color)
+function PlotAgents(nagent,AGENT,Color,Marking)
 % plot agents as circles with the size of the circle being their radius
 % AGENT is a structure that needs to have the following fields:
 % .Size: radius of the agent
@@ -10,6 +10,8 @@ function PlotAgents(nagent,AGENT,Color)
 %
 % Marcel Thielmann Oct 2011
 
+
+
 for i = 1:nagent
     radius = AGENT(i).Size;
     x      = AGENT(i).LocX;
@@ -19,5 +21,9 @@ for i = 1:nagent
     catch
         bla =1;
     end
-    text(x,y,[num2str(AGENT(i).name)],'HorizontalAlignment','center')
+    
+    if strcmp(Marking,'number'); agentText = [num2str(AGENT(i).name)];
+    elseif strcmp(Marking,'smiley'); agentText = ':-)';
+    end
+    text(x,y,agentText,'HorizontalAlignment','center','VerticalAlignment','middle','FontSize',12)
 end
