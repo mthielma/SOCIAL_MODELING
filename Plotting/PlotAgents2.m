dbff4bf052fc698b2bb87c31750f5b5c3fda2ad3
Fabio <fabio.crameri@erdw.ethz.ch>
2011-11-30 16:48:48 +0100

    -update

--- Plotting/PlotAgents2.m
+++ Plotting/PlotAgents2.m
@@ -1,12 +1,16 @@
-function PlotAgents(nagent,AGENT,Color,Marking)
+function PlotAgents(nagent,AGENT,PLOTTING)
 % plot agents as circles with the size of the circle being their radius
+% 
+% nagent: number of agents
+% 
 % AGENT is a structure that needs to have the following fields:
 % .Size: radius of the agent
 % .LocX: x-location
 % .LocY: y-location
 %
-% nagent: number of agents
-% Color: face color of the marker
+% PLOTTING is a structure that needs to have the following fields:
+% .Marking: defines agents description
+% .Color: face color of the marker
 %
 % Marcel Thielmann Oct 2011
 
@@ -17,13 +21,14 @@ for i = 1:nagent
     x      = AGENT(i).LocX;
     y      = AGENT(i).LocY;
     try
-    rectangle('position',[x-radius, y-radius, 2*radius, 2*radius],'curvature',[1 1],'FaceColor',Color);
+    rectangle('position',[x-radius, y-radius, 2*radius, 2*radius],'curvature',[1 1],'FaceColor',PLOTTING.Color);
     catch
         bla =1;
     end
     
-    if strcmp(Marking,'number'); agentText = [num2str(AGENT(i).name)];
-    elseif strcmp(Marking,'smiley'); agentText = ':-)';
+    if strcmp(PLOTTING.Marking,'no');
+    elseif strcmp(PLOTTING.Marking,'number'); agentText = [num2str(AGENT(i).name)];
+    elseif strcmp(PLOTTING.Marking,'smiley'); agentText = ':-)';
     end
-    text(x,y,agentText,'HorizontalAlignment','center','VerticalAlignment','middle','FontSize',12)
+    text(x,y,agentText,'HorizontalAlignment','center','VerticalAlignment','middle','FontSize',11)
 end
