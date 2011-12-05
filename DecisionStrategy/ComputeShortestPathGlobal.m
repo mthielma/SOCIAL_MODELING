4e5442edb0f483077c90139bd35bc90068808e68
Marcel Thielmann <marcel.thielmann@erdw.ethz.ch>
2011-12-05 15:13:02 +0100

    changed the shortest path computation functions

--- DecisionStrategy/ComputeShortestPathGlobal.m
+++ DecisionStrategy/ComputeShortestPathGlobal.m
@@ -1,23 +1,23 @@
-function [Dgradx,Dgrady] = ComputeShortestPathGlobal(BuildingList,Xexit,Yexit,X_Grid,Y_Grid,TopoGradientX,TopoGradientY,v0,resolution)
+function [Dgradx,Dgrady,D] = ComputeShortestPathGlobal(BuildingMap,ExitMap,X_Grid,Y_Grid,v0,resolution)
 
 Debug =false;
 
 % set initial speed map
 F = ones(size(X_Grid))*v0;
 % add buildings to map
-for i=1:size(BuildingList,1)
-    F(X_Grid>=BuildingList(i,1) & X_Grid<=BuildingList(i,2) & Y_Grid>=BuildingList(i,3) & Y_Grid<=BuildingList(i,4)) = 1e-8;
-end
+% add buildings to map
+F(BuildingMap) = 1e-8;
 
 %==============================================================
-% FIRST GUESS
+% Shortest path w/o topo
 %==============================================================
 
-% find indices of exit
-[indx,indy] = find(X_Grid==Xexit & Y_Grid==Yexit);
+% find indices of exits
+[indx,indy] = find(ExitMap == 1);
+ExitPoints = [indx';indy'];
 
 % use fast marching algorithm to compute distance to exit
-[D]=msfm(F, [indx indy]');
+[D]=msfm(F, ExitPoints);
 
 % compute gradient in time -> this gives us the direction
 [Dgradx,Dgrady] = gradient(D,resolution,resolution);
@@ -27,36 +27,7 @@ Dgradtot = sqrt(Dgradx.^2+Dgrady.^2);
 Dgradx   = -Dgradx./Dgradtot;
 Dgrady   = -Dgrady./Dgradtot;
 
-
 % this could be done in an iterative manner, but is this necessary?
-
-if sum(TopoGradientX(:))+sum(TopoGradientY(:))>0
-    %==============================================================
-    % RECOMPUTE WITH TOPOGRAPHY
-    %==============================================================
-    
-    % recompute the velocity field based on the slope
-    slope = TopoGradientX.*Dgradx+TopoGradientY.*Dgrady;
-    
-    % compute maximum velocity due to slope
-    PreFac = (v0./exp(-3.5*0.05));
-    F = PreFac.*exp(-3.5.*abs(slope+0.05));
-    % add buildings to map
-    for i=1:size(BuildingList,1)
-        F(X_Grid>=BuildingList(i,1) & X_Grid<=BuildingList(i,2) & Y_Grid>=BuildingList(i,3) & Y_Grid<=BuildingList(i,4)) = 1e-8;
-    end
-    
-    % use fast marching algorithm to compute distance to exit
-    [D]=msfm(F, [indx indy]');
-    
-    % compute gradient in time -> this gives us the direction
-    [Dgradx,Dgrady] = gradient(D,resolution,resolution);
-    
-    % scale direction vectors to normal
-    Dgradtot = sqrt(Dgradx.^2+Dgrady.^2);
-    Dgradx   = -Dgradx./Dgradtot;
-    Dgrady   = -Dgrady./Dgradtot;    
-end
 
 if Debug
     
--- DecisionStrategy/ComputeShortestPathGlobalWithAgents.m
+++ DecisionStrategy/ComputeShortestPathGlobalWithAgents.m
@@ -1,89 +1,86 @@
-function [Dgradx,Dgrady] = ComputeShortestPathGlobalWithAgents(BuildingList,Xexit,Yexit,X_Grid,Y_Grid,TopoGradientX,TopoGradientY,AGENT,nagent,Parameter)
-
+function [Dgradx,Dgrady] = ComputeShortestPathGlobalWithAgents(BuildingMap,ExitMap,X_Grid,Y_Grid,Z_Grid,D_orig,AGENT,nagent,Parameter)
 Debug = false;
 
+BuildingMap = logical(BuildingMap);
 % set initial speed map
-F = ones(size(X_Grid))*Parameter.v0;
+F_agent = ones(size(X_Grid))*Parameter.v0;
+
 % add buildings to map
-for i=1:size(BuildingList,1)
-    F(X_Grid>=BuildingList(i,1) & X_Grid<=BuildingList(i,2) & Y_Grid>=BuildingList(i,3) & Y_Grid<=BuildingList(i,4)) = 1e-8;
-end
+F_agent(BuildingMap) = 1e-8;
 
 %==============================================================
-% FIRST GUESS
+% Exits
 %==============================================================
 
 % find indices of exits
-ExitPoints = zeros(2,size(Xexit,1));
-for i = 1:size(Xexit,1)
-    [indx,indy] = find(X_Grid==Xexit(i) & Y_Grid==Yexit(i));
-    ExitPoints(1,i) = indx;
-    ExitPoints(2,i) = indy;
-end
-% use fast marching algorithm to compute distance to exit
-[D_orig]=msfm(F, ExitPoints);
-
-% compute gradient in time -> this gives us the direction
-[Dgradx_orig,Dgrady_orig] = gradient(D_orig,Parameter.resolution,Parameter.resolution);
-
-% scale direction vectors to normal
-Dgradtot = sqrt(Dgradx_orig.^2+Dgrady_orig.^2);
-Dgradx_orig   = -Dgradx_orig./Dgradtot;
-Dgrady_orig   = -Dgrady_orig./Dgradtot;
+[indx,indy] = find(ExitMap == 1);
+ExitPoints = [indx';indy'];
 
 %==============================================================
-% RECOMPUTE WITH TOPOGRAPHY AND AGENTS
+% COMPUTE WITH AGENTS
 %==============================================================
-% recompute the velocity field based on the slope
-slope = TopoGradientX.*Dgradx_orig+TopoGradientY.*Dgrady_orig;
-
-% compute maximum velocity due to slope
-PreFac = (Parameter.v0./exp(-3.5*0.05));
-F = PreFac.*exp(-3.5.*abs(slope+0.05));
-% add buildings to map
-for i=1:size(BuildingList,1)
-    F(X_Grid>=BuildingList(i,1) & X_Grid<=BuildingList(i,2) & Y_Grid>=BuildingList(i,3) & Y_Grid<=BuildingList(i,4)) = 1e-8;
-end
-
-F0 = F;
 % find out at which node agents are located and reduce velocity at this
-% node by a constant factor (for now reduce it to 10 % of the actual velocity)
+% node by a constant factor (for now reduce it to 25 % of the actual velocity)
 % unfortunately, we have to loop over the agents so far...
 for iagent = 1:nagent
    Inside = (X_Grid-AGENT(iagent).LocX).*(X_Grid-AGENT(iagent).LocX)+(Y_Grid-AGENT(iagent).LocY).*(Y_Grid-AGENT(iagent).LocY);
-   F(Inside<AGENT(iagent).Size) = F0(Inside<AGENT(iagent).Size)*0.25;
+   F_agent(Inside<AGENT(iagent).Size) = F_agent(Inside<AGENT(iagent).Size)*1/Parameter.agent_sensitivity;
 end
 
-
 % use fast marching algorithm to compute distance to exit
-[D_agent]=msfm(F, ExitPoints);
+[D_agent]=msfm(F_agent, ExitPoints);
+%[D_test] = msfm2d_gradient(Z_Grid,Parameter.resolution,Parameter, ExitPoints,false,false);
+
+[Dgradx,Dgrady] = gradient(D_agent,Parameter.resolution,Parameter.resolution);
+
+Dgradtot = sqrt(Dgradx.^2+Dgrady.^2);
+Dgradx   = -Dgradx./Dgradtot;
+Dgrady   = -Dgrady./Dgradtot;
+
+% scale d_orig and D_agent to their respective max
+D_orig(~BuildingMap) = D_orig(~BuildingMap)./max(D_orig(~BuildingMap(:)));
+D_agent(~BuildingMap) = D_agent(~BuildingMap)./max(D_agent(~BuildingMap(:)));
 
 % compute mixture of both fields taking into account sensitivity to agents
 % in the way
-Dmix = (D_orig + Parameter.agent_sensitivity.*D_agent)./(Parameter.agent_sensitivity+1);
+Dmix = (D_agent+ Parameter.orig_sensitivity.*D_orig);
+
 [Dgradx,Dgrady] = gradient(Dmix,Parameter.resolution,Parameter.resolution);
 
 Dgradtot = sqrt(Dgradx.^2+Dgrady.^2);
 Dgradx   = -Dgradx./Dgradtot;
 Dgrady   = -Dgrady./Dgradtot;
 
+
+
+
 if Debug
 
     figure(99),clf
-    pcolor(X_Grid,Y_Grid,double(D)),shading interp, colorbar
+    subplot(131)
+    hold on
+    pcolor(X_Grid,Y_Grid,D_orig),shading flat, colorbar
+    %quiver(X_Grid,Y_Grid,Dgradx_orig,Dgrady_orig)
+    caxis([0 1])
+    axis equal,axis tight
+    subplot(132)
+    hold on
+    pcolor(X_Grid,Y_Grid,D_topo),shading flat, colorbar
+    %quiver(X_Grid,Y_Grid,Dgradx_topo,Dgrady_topo)
+     axis equal,axis tight
+    caxis([0 1])
+    subplot(133)
+    hold on
+    pcolor(X_Grid,Y_Grid,D_agent),shading flat, colorbar
+    %quiver(X_Grid,Y_Grid,Dgradx_agent,Dgrady_agent)
+     axis equal,axis tight
+    caxis([0 1])
+     
+    figure(98),clf
+    pcolor(X_Grid,Y_Grid,Dmix-D_orig),shading flat, colorbar
+    caxis([0 5])
     hold on
     quiver(X_Grid,Y_Grid,Dgradx,Dgrady,'w')
     
-    figure(98),clf
-    pcolor(X_Grid,Y_Grid,double(D)),shading interp, colorbar
-    hold on
-    quiver(X_Grid,Y_Grid,Dgradx_agent,Dgrady_agent,'w')
-    
-    figure(97),clf
-    pcolor(X_Grid,Y_Grid,double(D)),shading interp, colorbar
-    hold on
-    quiver(X_Grid,Y_Grid,Dgradx_orig,Dgrady_orig,'w')
-    
-    
-    
+     axis equal,axis tight
 end
