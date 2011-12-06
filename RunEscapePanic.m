98b46056ecc9dce43e6714271d64ae75bd7a77df
Fabio <fabio.crameri@erdw.ethz.ch>
2011-12-06 09:46:39 +0100

    -changes

--- RunEscapePanic.m
+++ RunEscapePanic.m
@@ -4,39 +4,42 @@ clear;
 % Workflow control
 Parameter.DirectExitPath = true; % true or false, determines if the fast marching algorithm is used for shortest path computation (false), or if the exit direction is the direct line (true)
 
-Parameter.WithAgents = false; % true or false, determines if agents are taken into account for shortest path computation
-Parameter.WithTopo   = false; % true or false, determines if topography is taken into account for shortest path computation
-Parameter.WithFlood  = false; % true or false, determines if flood is taken into account for shortest path computation
+Parameter.WithAgents    = false; % true or false, determines if agents are taken into account for shortest path computation
+Parameter.WithTopo      = false; % true or false, determines if topography is taken into account for shortest path computation
+Parameter.WithFlood     = false; % true or false, determines if flood is taken into account for shortest path computation
 
 % subfolder and topo information
-Foldername = 'test'; % subfolder where output is to be stored
-Topo_name  = 'none'; % topography file to be loaded (if there is none, use 'none')
+Foldername      = 'test1'; % subfolder where output is to be stored
+Topo_name       = 'none'; % topography file to be loaded (if there is none, use 'none')
+
+Parameter.PlotTimeStep  = 100;      % plots every PlotTimeStep
+Parameter.SaveTimeStep  = 100;      % saves every PlotTimeStep
 
 % domain
-Parameter.xmin                = 0;
-Parameter.xmax                = 20;
-Parameter.ymin                = 0;
-Parameter.ymax                = 10;
+Parameter.xmin          = 0;
+Parameter.xmax          = 20;
+Parameter.ymin        	= 0;
+Parameter.ymax       	= 10;
 % number of agents
-Parameter.nagent          	  = 50;      % number of agents
+Parameter.nagent      	= 50;      % number of agents
 
 %numerical parameter
-Parameter.resolution            = 0.1;      % resolution in [m]
+Parameter.resolution  	= 0.1;      % resolution in [m]
 
-Parameter.dt                	= 0.01;    	% time step in [s]
-Parameter.maxtime               = 3;       % maximum time to run in [min]
+Parameter.dt           	= 0.01;    	% time step in [s]
+Parameter.maxtime      	= 3;       % maximum time to run in [min]
 
-Parameter.pert_social           = 0.05;     % maximal amplitude factor of social agent forces perturbation
-Parameter.decision_time         = 0.01;      % after which time does an agent redecide on its path?
+Parameter.pert_social  	= 0.0;     % maximal amplitude factor of social agent forces perturbation e.g. 0.05
+Parameter.decision_time	= 0.01;      % after which time does an agent redecide on its path?
 
 % physical forces parameters (Helbing,2000)
-Parameter.PhysicalForces    = true;
-Parameter.Tangential        = false;
-Parameter.k                 = 1.2e7;
+Parameter.PhysicalForces	= true;
+Parameter.Tangential        = true;
+Parameter.k                 = 1.2e5;
 Parameter.kappa             = 2.4e5;
 
 % social force parameters
-Parameter.SocialForces   = true;
+Parameter.SocialForces	= true;
 Parameter.A             = 2e3;   	%[N]  [2e3 Helbing 2000]
 Parameter.B             = 0.08;     %[m]  [0.08 Helbing 2000]
 Parameter.ExitFactor    = 1;        %for adjusting strength of constant exit force field
@@ -54,8 +57,8 @@ Parameter.slope_crit = 0.05;
 % agent parameters (can be perturbed with a random perturbation)
 Parameter.m                 = 80;       % mass in kg
 Parameter.m_pert            = 10;
-Parameter.v0               	= 5;        % maximal/desired velocity [m/s]
-Parameter.v0_pert           = 1;   
+Parameter.v0               	= 0.5;        % maximal/desired velocity [m/s]
+Parameter.v0_pert           = 0;   
 
 Parameter.t_acc            	= 0.5;      % acceleration time in [s]
 
@@ -96,7 +99,7 @@ ExitList = [
 %---------------------------------------
 % test map
 %---------------------------------------
-% TestMap(Parameter,BuildingList,ExitList,filename,topo_name);      
+% TestMap(Parameter,BuildingList,ExitList,Foldername,Topo_name);      
        
 %---------------------------------------
 % run simulation
