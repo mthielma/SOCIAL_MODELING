3a8e47e464ddc96c89961a3acd6dc0ec40cef89a
Marcel Thielmann <marcel.thielmann@erdw.ethz.ch>
2011-12-06 10:47:09 +0100

    - fixed a major bug and added wall force direction

--- WallForces/ArchitectureForceV2.m
+++ WallForces/ArchitectureForceV2.m
@@ -1,36 +1,54 @@
-function [ForceTotal,DirX,DirY] = ArchitectureForceV2(X_Grid,Y_Grid,List,Parameter,resolution)
+function [ArchForce,DirX,DirY] = ArchitectureForceV2(X_Grid,Y_Grid,Map,Parameter,resolution)
 
 % compute architecture forces
 
 % loop through building list and compute the social force from each
 % building
 % then add up all forces and set forces inside buildings to 0
+ArchForce = zeros(size(X_Grid));
+ForceX       = zeros(size(X_Grid));
+ForceY       = zeros(size(X_Grid));
 
-for i=1:size(List,1)
-    MapTemp = zeros(size(X_Grid));
-    MapTemp(X_Grid>=List(i,1) & X_Grid<=List(i,2) & Y_Grid>=List(i,3) & Y_Grid<=List(i,4)) = 1;
-    % we use bwdist to compute the distance to the first nonzero pixel
-    [D,L] = bwdist(MapTemp); 
-    D = double(D).*resolution; % distance is given in pixels, which is why we have to transform it to real distances
-    % compute the direction vector from each pixel to its nearest nonzero
-    % pixel
-    DirX = 0*X_Grid;
-    DirY = 0*X_Grid;
-    DirX(:) = X_Grid(:) - X_Grid(L(:)); % actual distance between actual point and nearest building point
-    DirY(:) = Y_Grid(:) - Y_Grid(L(:)); % actual distance between actual point and nearest building point
-    DirTot = sqrt(DirX.*DirX+DirY.*DirY);
-    DirX = DirX./DirTot;
-    DirY = DirY./DirTot;
-    
-    DirX(DirTot==0) = 0;
-    DirY(DirTot==0) = 0;
-    
-    ForceTotal   = Parameter.A.*exp(-D./Parameter.B);
-    
-end
 
-% remove forces inside buildings
-for i=1:size(List,1)
-    ForceTotal(X_Grid>=List(i,1) & X_Grid<=List(i,2) & Y_Grid>=List(i,3) & Y_Grid<=List(i,4)) = 0;
-end
+[D,L] = bwdist(Map);
+
+D = double(D).*resolution; % distance is given in pixels, which is why we have to transform it to real distances
+% compute the direction vector from each pixel to its nearest nonzero
+% pixel
+DirX = 0*X_Grid;
+DirY = 0*X_Grid;
+DirX(:) = X_Grid(:) - X_Grid(L(:)); % actual distance between actual point and nearest building point
+DirY(:) = Y_Grid(:) - Y_Grid(L(:)); % actual distance between actual point and nearest building point
+
+DirTot = sqrt(DirX.*DirX+DirY.*DirY);
+DirX = DirX./DirTot;
+DirY = DirY./DirTot;
+
+ArchForce   = Parameter.A.*exp(-D./Parameter.B);
+% buildings
+ArchForce(logical(Map)) = 0;
+
+
+% invert map
+MapInv = ~(logical(Map));
+[D,L] = bwdist(MapInv);
+D = double(D).*resolution;
+
+DirX_B = 0*X_Grid;
+DirY_B = 0*X_Grid;
+DirX_B(:) = -(X_Grid(:) - X_Grid(L(:))); % actual distance between actual point and nearest building point
+DirY_B(:) = -(Y_Grid(:) - Y_Grid(L(:))); % actual distance between actual point and nearest building point
+
+DirTot_B = sqrt(DirX_B.*DirX_B+DirY_B.*DirY_B);
+DirX_B = DirX_B./DirTot_B;
+DirY_B = DirY_B./DirTot_B;
+
+
+% treat buildings
+ArchForce(logical(Map)) = max(ArchForce(:));
+
+DirX(logical(Map)) = DirX_B(logical(Map));
+DirY(logical(Map)) = DirY_B(logical(Map));
+
+
 
