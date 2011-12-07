8f5baafea9f01f01a52b81790250f9bd03b25387
Marcel Thielmann <marcel.thielmann@erdw.ethz.ch>
2011-12-07 11:09:57 +0100

    added agent location correction

--- EscapePanic.m
+++ EscapePanic.m
@@ -123,7 +123,7 @@ y_Buildings = Y_Grid(BuildingMap);
 %----------------------------------------------------
 % compute forces from buildings (static)
 %----------------------------------------------------
-[ArchForce,ArchDirX,ArchDirY] = ArchitectureForceV2(X_Grid,Y_Grid,BuildingMap,Parameter,resolution);
+[ArchForce,ArchD,ArchDirX,ArchDirY] = ArchitectureForceV2(X_Grid,Y_Grid,BuildingMap,Parameter,resolution);
 
 %----------------------------------------------------
 % compute shortest path to exit
@@ -322,6 +322,15 @@ while (time <= maxtime && size(AGENT,2)>0)
     % move agents
     %----------------------------------------------------
     [AGENT] = MoveAgents(AGENT,X_Grid,Y_Grid,Gradient_x,Gradient_y,Parameter.dt,nagent,Parameter);
+    
+    
+    
+    
+    %----------------------------------------------------
+    % check if agents are inside walls and move them out
+    %----------------------------------------------------
+    
+    AGENT = CheckAgentsInBuildings(AGENT,BuildingList,X_Grid,Y_Grid,ArchDirX,ArchDirY,ArchD);
     
     %----------------------------------------------------
     % remove successfull agents
