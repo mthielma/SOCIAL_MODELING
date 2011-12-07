070e034176a3facf25541aa6c6d66cbcc37f17a5
Fabio <fabio.crameri@erdw.ethz.ch>
2011-12-07 16:37:03 +0100

    removed

--- ComputeSocialForcesDynamic.m
+++
@@ -1,8 +0,0 @@
-function [FxDynamicSocial,FyDynamicSocial] = ComputeSocialForcesDynamic(Parameter,DistanceToAgents,NormalVector)
-
-% compute social force
-F_socAgents     = Parameter.A.*exp(DistanceToAgents./Parameter.B);
-
-FxDynamicSocial    = F_socAgents.*NormalVector(:,1);
-FyDynamicSocial    = F_socAgents.*NormalVector(:,2);
-
