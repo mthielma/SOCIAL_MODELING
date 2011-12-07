05f241af445bebc4771441af0cf425438de1a8a1
Fabio <fabio.crameri@erdw.ethz.ch>
2011-12-07 11:56:33 +0100

    -update

--- Plotting/PlotAgents.m
+++ Plotting/PlotAgents.m
@@ -1,22 +1,33 @@
-function PlotAgents(nagent,AGENT,Color)
+function PlotAgents(nagent,AGENT,Plotting)
 % plot agents as circles with the size of the circle being their radius
+% 
+% nagent: number of agents
+% 
 % AGENT is a structure that needs to have the following fields:
 % .Size: radius of the agent
 % .LocX: x-location
 % .LocY: y-location
 %
-% nagent: number of agents
-% Color: face color of the marker
+% Plotting is a structure that needs to have the following fields:
+% .Marking: defines agents description
+% .Color: face color of the marker
 %
 % Marcel Thielmann Oct 2011
+
 
 for i = 1:nagent
     radius = AGENT(i).Size;
     x      = AGENT(i).LocX;
     y      = AGENT(i).LocY;
     try
-    rectangle('position',[x-radius, y-radius, 2*radius, 2*radius],'curvature',[1 1],'FaceColor',Color);
+    rectangle('position',[x-radius, y-radius, 2*radius, 2*radius],'curvature',[1 1],'FaceColor',Plotting.Color);
     catch
-        bla =1;
+        bla=1;
     end
+    
+    if strcmp(Plotting.Marking,'no');
+    elseif strcmp(Plotting.Marking,'number'); agentText = [num2str(AGENT(i).name)];
+    elseif strcmp(Plotting.Marking,'smiley'); agentText = ':-)';
+    end
+    text(x,y,agentText,'HorizontalAlignment','center','VerticalAlignment','middle','FontSize',11)
 end
